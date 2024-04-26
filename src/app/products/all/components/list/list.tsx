baf13d86ab79adb722ffac620133b3bbc39f2f11
Cristian <crissnar12xd@gmail.com>
2024-04-26 00:39:24 -0500

    add pagination

--- src/app/products/all/components/list/list.tsx
+++ src/app/products/all/components/list/list.tsx
@@ -3,11 +3,34 @@ import useModal from '../add/hooks/useModal'
 import { useProductStore } from '../../../store/use.products.store'
 import { ProductCard } from './components'
 import UploadForm from '../add/add'
+import { useState, useEffect } from 'react';
 
 export const List = () => {
-  const { products } = useProductStore()
-
   const { openModal, isOpen, closeModal } = useModal()
+  const { products, loading, getAllProducts } = useProductStore();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Función para cargar los productos de la página actual
+  const loadProducts = async () => {
+    const offset = (currentPage - 1) * 10; // Cálculo del offset para la página actual
+     getAllProducts({ offset, limit: 10 }); // Obtener productos con el offset y el límite correspondientes
+  };
+
+  // Manejador de clic en el botón de página anterior
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  // Manejador de clic en el botón de página siguiente
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  useEffect(() => {
+    loadProducts(); // Cargar los productos cuando cambie la página actual
+  }, [currentPage]); // Ejecutar la carga cuando cambie la página actual
 
   return (
     <>
@@ -20,12 +43,29 @@ export const List = () => {
       </button>
       <div className='container mx-auto py-8 bg-white rounded-md shadow-xl mb-10'>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center'>
-          {products.map((product) => (
-            <ProductCard
-              key={product.id}
-              product={product}
-            />
-          ))}
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          )}
+        </div>
+        {/* Botones de paginación */}
+        <div className='flex justify-center mt-4'>
+          <button
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+            className='mr-4 px-3 py-1 bg-gray-200 rounded bg-primary text-white'
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextPage}
+            className='px-3 py-1 bg-gray-200 rounded bg-primary text-white'
+          >
+            Next
+          </button>
         </div>
       </div>
 
@@ -36,5 +76,6 @@ export const List = () => {
         productToEdit={undefined}
       />
     </>
-  )
-}
+  );
+};
+
