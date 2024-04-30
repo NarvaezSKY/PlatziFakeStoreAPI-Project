f842716aa77d0ed2cbdbb540a3160057a44cb2cb
Cristian <crissnar12xd@gmail.com>
2024-04-30 10:58:46 -0500

    deleting comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import ProductsPage from './app/products/pages/AllProducts'
-import ProductDetails from './app/products/details/index' // Importa el componente ProductDetails
+import ProductDetails from './app/products/details/index'
 import AllCategoriesPage from './app/categories/all'
 import CategoryProductsPage from './app/categories/productsByCategory/ProductsByCategory'
 import AllProductsPage from './app/products/all'
